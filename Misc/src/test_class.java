a8079dbe36730d1accc2d718e514541e378d04a1
scduffy <scduffy@loyola.edu>
2018-01-16 15:47:08 -0500

    no

--- Misc/src/test_class.java
+++ Misc/src/test_class.java
@@ -3,8 +3,6 @@ public class test_class
 	public static void main (String [] args)
 	{
 		System.out.println("Hello World!");
-		System.out.println("Hello Again World!");
-		//comment
-		
+		System.out.println("Hello Again World!");	
 	}
 }
